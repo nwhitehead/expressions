34646b833de84afba2db704830917fbc890fe032
Nathan Whitehead <nwhitehe@gmail.com>
2024-12-16 15:55:37 -0700

    Some changes.

--- nathan_api.mjs
+++ nathan_api.mjs
@@ -12,7 +12,7 @@ const prompt = {
       "rotate_pitch": 0,
       "rotate_yaw": 0,
       "rotate_roll": 0,
-      "blink": 0,
+      "blink": -20,
       "eyebrow": 0,
       "wink": 0,
       "pupil_x": 0,
@@ -55,51 +55,47 @@ const prompt = {
   }
 };
 
-const ws = new WebSocket(`ws://${SERVER_ADDRESS}/ws?clientId=${CLIENT_ID}`);
+async function queue_prompt(prompt) {
 
-async function queue_prompt(ws, prompt) {
+    const ws = new WebSocket(`ws://${SERVER_ADDRESS}/ws?clientId=${CLIENT_ID}`);
 
-    const inputImgData = fs.readFileSync('katia.jpg');
+    const inputImgData = fs.readFileSync('images/katia.jpg');
     const b64data = Buffer.from(inputImgData, 'binary').toString('base64');
     prompt["3"]["inputs"]["image"] = b64data;
 
     const p = { prompt, client_id: CLIENT_ID };
     const data = JSON.stringify(p);
-    try {
-        const response = await axios.post(`http://${SERVER_ADDRESS}/prompt`, data);
-        const prompt_id = response.data.prompt_id;
-        console.log(`Started workflow with prompt_id=${prompt_id} client_id=${CLIENT_ID}`);
-        // Wait for progress message for right node (1 image output)
-        let ready = false;
-        ws.onmessage = function incoming(msg) {
-            if (typeof msg.data === 'string') {
-                const data = JSON.parse(msg.data);
-                if (data.type === 'progress' && data.data.prompt_id === prompt_id && data.data.node === 'save_image_websocket_node' && data.data.value === 0) {
-                    ready = true;
+    ws.onopen = async function () {
+        try {
+            const response = await axios.post(`http://${SERVER_ADDRESS}/prompt`, data);
+            const prompt_id = response.data.prompt_id;
+            console.log(`Started workflow with prompt_id=${prompt_id} client_id=${CLIENT_ID}`);
+            // Wait for progress message for right node (1 image output)
+            let ready = false;
+            ws.onmessage = function incoming(msg) {
+                if (typeof msg.data === 'string') {
+                    const data = JSON.parse(msg.data);
+                    if (data.type === 'progress' && data.data.prompt_id === prompt_id && data.data.node === 'save_image_websocket_node' && data.data.value === 0) {
+                        ready = true;
+                    } else if (data.type === 'executing' && data.data.prompt_id === prompt_id && data.data.node === null) {
+                        console.log('Done');
+                    } else {
+                        ready = false;
+                    }
                 } else {
-                    ready = false;
+                    // Handle binary image output data
+                    if (ready) {
+                        // Ignore first 8 bytes, they are 0x1 0x2 (not sure why)
+                        fs.writeFileSync('images/out.png', msg.data.slice(8));
+                        ready = false;
+                    }
                 }
-            } else {
-                // Handle binary image output data
-                if (ready) {
-                    console.log(`binary ${msg.data.constructor.name}`);
-                    // Ignore first 8 bytes, they are 0x1 0x2 (not sure why)
-                    fs.writeFileSync('out.png', msg.data.slice(8));
-                }
-            }
-        };
-    } catch(error) {
-        console.log('error', error);
-    }
-    // req =  urllib.request.Request("http://{}/prompt".format(server_address), data=data)
-    // return json.loads(urllib.request.urlopen(req).read())
+            };
+        } catch(error) {
+            console.log('error', error);
+        }
+    };
+    console.log('Returning from queue_prompt');
 }
 
-ws.onopen = function open() {
-    console.log('connected');
-    queue_prompt(ws, prompt);
-};
-
-ws.onclose = function close() {
-    console.log('disconnected');
-};
+queue_prompt(prompt);
