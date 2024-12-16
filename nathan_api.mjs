563fca2f79b867c5640f4462bb581559fcea3ef5
Nathan Whitehead <nwhitehe@gmail.com>
2024-12-16 16:05:30 -0700

    Use promises to finish queue_prompt.

--- nathan_api.mjs
+++ nathan_api.mjs
@@ -1,3 +1,4 @@
+import process from 'node:process';
 import fs from 'node:fs';
 import crypto from 'crypto';
 import WebSocket from 'isomorphic-ws';
@@ -55,47 +56,55 @@ const prompt = {
   }
 };
 
-async function queue_prompt(prompt) {
+function queue_prompt(prompt) {
+    return new Promise((resolve, reject) => {
+        const ws = new WebSocket(`ws://${SERVER_ADDRESS}/ws?clientId=${CLIENT_ID}`);
 
-    const ws = new WebSocket(`ws://${SERVER_ADDRESS}/ws?clientId=${CLIENT_ID}`);
-
-    const inputImgData = fs.readFileSync('images/katia.jpg');
-    const b64data = Buffer.from(inputImgData, 'binary').toString('base64');
-    prompt["3"]["inputs"]["image"] = b64data;
-
-    const p = { prompt, client_id: CLIENT_ID };
-    const data = JSON.stringify(p);
-    ws.onopen = async function () {
-        try {
-            const response = await axios.post(`http://${SERVER_ADDRESS}/prompt`, data);
-            const prompt_id = response.data.prompt_id;
-            console.log(`Started workflow with prompt_id=${prompt_id} client_id=${CLIENT_ID}`);
-            // Wait for progress message for right node (1 image output)
-            let ready = false;
-            ws.onmessage = function incoming(msg) {
-                if (typeof msg.data === 'string') {
-                    const data = JSON.parse(msg.data);
-                    if (data.type === 'progress' && data.data.prompt_id === prompt_id && data.data.node === 'save_image_websocket_node' && data.data.value === 0) {
-                        ready = true;
-                    } else if (data.type === 'executing' && data.data.prompt_id === prompt_id && data.data.node === null) {
-                        console.log('Done');
+        const inputImgData = fs.readFileSync('images/katia.jpg');
+        const b64data = Buffer.from(inputImgData, 'binary').toString('base64');
+        prompt["3"]["inputs"]["image"] = b64data;
+    
+        const p = { prompt, client_id: CLIENT_ID };
+        const data = JSON.stringify(p);
+        ws.onopen = async function () {
+            try {
+                const response = await axios.post(`http://${SERVER_ADDRESS}/prompt`, data);
+                const prompt_id = response.data.prompt_id;
+                // Wait for progress message for right node (1 image output)
+                let ready = false;
+                ws.onmessage = function incoming(msg) {
+                    if (typeof msg.data === 'string') {
+                        const data = JSON.parse(msg.data);
+                        if (data.type === 'progress' && data.data.prompt_id === prompt_id && data.data.node === 'save_image_websocket_node' && data.data.value === 0) {
+                            ready = true;
+                        } else if (data.type === 'executing' && data.data.prompt_id === prompt_id && data.data.node === null) {
+                            // Resolve queue_prompt promise
+                            resolve();
+                        } else {
+                            ready = false;
+                        }
                     } else {
-                        ready = false;
-                    }
-                } else {
-                    // Handle binary image output data
-                    if (ready) {
-                        // Ignore first 8 bytes, they are 0x1 0x2 (not sure why)
-                        fs.writeFileSync('images/out.png', msg.data.slice(8));
-                        ready = false;
+                        // Handle binary image output data
+                        if (ready) {
+                            // Ignore first 8 bytes, they are 0x1 0x2 (not sure why)
+                            fs.writeFileSync('images/out.png', msg.data.slice(8));
+                            console.log('Wrote images/out.png');
+                            ready = false;
+                        }
                     }
                 }
-            };
-        } catch(error) {
-            console.log('error', error);
-        }
-    };
-    console.log('Returning from queue_prompt');
+            } catch(error) {
+                console.log(error);
+                // Reject queue_prompt promise
+                reject();
+            }
+        };
+    });
 }
 
-queue_prompt(prompt);
+async function main() {
+    await queue_prompt(prompt);
+    process.exit(0);
+}
+
+main()
