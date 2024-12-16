8000b8c321131d129d9944e05489e9e2b1c1eacb
Nathan Whitehead <nwhitehe@gmail.com>
2024-12-16 13:03:31 -0700

    Working test call.

--- nathan_api.mjs
+++ nathan_api.mjs
@@ -6,8 +6,7 @@ import axios from 'axios';
 const SERVER_ADDRESS = "127.0.0.1:8188";
 const CLIENT_ID = crypto.randomUUID();
 
-const prompt = `
-{
+const prompt = {
   "2": {
     "inputs": {
       "rotate_pitch": 0,
@@ -54,32 +53,53 @@ const prompt = `
       ]
     }
   }
-}
-`;
+};
 
-
-// function queue_prompt(prompt) {
-//     const p = { prompt, client_id: CLIENT_ID };
-//     const data = JSON.stringify(p);
-//     req =  urllib.request.Request("http://{}/prompt".format(server_address), data=data)
-//     return json.loads(urllib.request.urlopen(req).read())
-
-// }
-
-
-// const ws = new WebSocket('wss://websocket-echo.com/');
 const ws = new WebSocket(`ws://${SERVER_ADDRESS}/ws?clientId=${CLIENT_ID}`);
+
+async function queue_prompt(ws, prompt) {
+
+    const inputImgData = fs.readFileSync('katia.jpg');
+    const b64data = Buffer.from(inputImgData, 'binary').toString('base64');
+    prompt["3"]["inputs"]["image"] = b64data;
+
+    const p = { prompt, client_id: CLIENT_ID };
+    const data = JSON.stringify(p);
+    try {
+        const response = await axios.post(`http://${SERVER_ADDRESS}/prompt`, data);
+        const prompt_id = response.data.prompt_id;
+        console.log(`Started workflow with prompt_id=${prompt_id} client_id=${CLIENT_ID}`);
+        // Wait for progress message for right node (1 image output)
+        let ready = false;
+        ws.onmessage = function incoming(msg) {
+            if (typeof msg.data === 'string') {
+                const data = JSON.parse(msg.data);
+                if (data.type === 'progress' && data.data.prompt_id === prompt_id && data.data.node === 'save_image_websocket_node' && data.data.value === 0) {
+                    ready = true;
+                } else {
+                    ready = false;
+                }
+            } else {
+                // Handle binary image output data
+                if (ready) {
+                    console.log(`binary ${msg.data.constructor.name}`);
+                    // Ignore first 8 bytes, they are 0x1 0x2 (not sure why)
+                    fs.writeFileSync('out.png', msg.data.slice(8));
+                }
+            }
+        };
+    } catch(error) {
+        console.log('error', error);
+    }
+    // req =  urllib.request.Request("http://{}/prompt".format(server_address), data=data)
+    // return json.loads(urllib.request.urlopen(req).read())
+}
 
 ws.onopen = function open() {
     console.log('connected');
-    ws.send(Date.now());
+    queue_prompt(ws, prompt);
 };
 
 ws.onclose = function close() {
     console.log('disconnected');
-};
-
-ws.onmessage = function incoming(data) {
-    console.log(`Roundtrip time: ${Date.now() - data.data} ms\n${JSON.stringify(data)}`);
-    ws.close();
 };
